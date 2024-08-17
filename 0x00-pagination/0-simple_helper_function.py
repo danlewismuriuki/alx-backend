a42d65544f27fbfb992d3e7863442f2bc2d0319f
Dan <danlewismuriuki@gmail.com>
2024-08-17 21:36:13 +0300

    Fix index_range function to calculate correct pagination indices
    - Corrected the calculation of `end_index` in the `index_range` function.
    - The function now correctly returns a tuple containing the start and end indices for pagination based on the given `page` and `page_size`.
    
    Previously, `end_index` was calculated using `start_index + page`, which was incorrect. It has been updated to use `start_index + page_size` to accurately reflect the end index for pagination.

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+function should return a tuple of size two containing
+start index and an end index corresponding to the range
+of indexes to return in a list for those particular
+pagination parameters.
+"""
+
+
+def index_range(page, page_size):
+    """function should return a tuple of size two
+        containing a start index and an end index
+    """
+
+    start_index = (page-1) * page_size
+    end_index = start_index + page
+
+    return (start_index, end_index)

01faca56061372a8bbb16751df4cd9c1cf0e878e
lsd <danlewismuriuki2@gmail.com>
2024-09-25 21:24:00 +0300

    feat(queue): implement job creation and processing for push notifications
    - Initialized a queue for handling push notification jobs.
    - Added functionality to create a job with phone number and message data.
    - Defined a job processor to send notifications asynchronously.
    - Implemented `sendNotification` function to handle notification delivery.
    - Ensured proper completion handling of jobs using the `done()` callback.

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -3,11 +3,11 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 function sendNotification(phoneNumber, message) {
-    console.log(`Sending notification to ${PHONE_NUMBER}, with message: ${MESSAGE}`);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 };
 
 queue.process('push_notification_code', (job, done) => {
-    const { phoneNumber, message } = jb.data;
+    const { phoneNumber, message } = job.data;
     sendNotification(phoneNumber, message);
     done();
 });
